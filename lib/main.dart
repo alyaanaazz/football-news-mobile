d67ab549abe47890e2471aef849a71d876d0083c
Nisrina Alya Nabilah <nisrinaalyanabilah@gmail.com>
2025-11-12 16:24:32 +0700

    tutorial 8 selesai

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:football_news/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +13,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Football News App', 
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
-            .copyWith(secondary: Colors.blueAccent[400], primary: Colors.blue),
+    return Provider(
+      create: (_){
+        CookieRequest request = CookieRequest();
+        return request;
+      },
+      child: MaterialApp(
+        title: 'Football News App', 
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
+              .copyWith(secondary: Colors.blueAccent[400], primary: Colors.blue),
+        ),
+        home: const LoginPage(),
       ),
-      home: MyHomePage(),
     );
   }
 }
--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
+import 'package:football_news/screens/news_entry_list.dart';
 import 'package:football_news/screens/newslist_form.dart';
 
 class LeftDrawer extends StatelessWidget {
@@ -28,7 +29,7 @@ class LeftDrawer extends StatelessWidget {
                 ),
                 Padding(padding: EdgeInsets.all(10)),
                 Text("Seluruh berita sepak bola terkini di sini!",
-                    // TODO: Tambahkan gaya teks dengan center alignment, font ukuran 15, warna putih, dan weight biasa
+                    // Tambahkan gaya teks dengan center alignment, font ukuran 15, warna putih, dan weight biasa
                     ),
               ],
             ),
@@ -47,22 +48,16 @@ class LeftDrawer extends StatelessWidget {
             },
           ),
           ListTile(
-            leading: const Icon(Icons.post_add),
-            title: const Text('Add News'),
-            // Bagian redirection ke NewsFormPage
+            leading: const Icon(Icons.add_reaction_rounded),
+            title: const Text('News List'),
             onTap: () {
-              /*
-              Buatlah routing ke NewsFormPage di sini,
-              setelah halaman NewsFormPage sudah dibuat.
-              */
-              Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const NewsFormPage(), // Gunakan const jika NewsFormPage tidak memiliki parameter yang berubah
-              ),
-            );
+                // Route to news list page
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const NewsEntryListPage()),
+                );
             },
-          ),
+        ),
         ],
       ),
     );
